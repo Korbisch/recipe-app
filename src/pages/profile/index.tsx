1fe97fb36342f0700cc96218a1f00df0afdb992d
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-04-08 22:49:45 +0200

    Create login functionality with Auth0

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+export default function Profile() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    <>
+      <h1>Profil</h1>
+      {user && (
+        <div>
+          <img src={user.picture ?? ""} alt={user.name ?? ""} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
+    </>
+  );
+}
