73da351d4ddafee079a98c23e52319dbfe54d055
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-04-17 21:08:48 +0200

    Don't zoom on input fields

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,7 +57,7 @@ export default function App(props: AppProps) {
         <title>PlateMate</title>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
       </Head>
 
